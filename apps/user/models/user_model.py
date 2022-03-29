0a858d26026b8054df86776144314acc7e72ec59
Marcos Santos <marcos.diego.ns@outlook.com>
2022-03-29 09:06:33 -0500

    first name and last name required

--- apps/user/models/user_model.py
+++ apps/user/models/user_model.py
@@ -6,4 +6,8 @@ from django.utils.translation import ugettext_lazy as _
 class User(AbstractUser):
     """Custom user model"""
 
+    first_name = models.CharField(_('first name'), max_length=30, blank=False, null=False)
+    last_name = models.CharField(_('last name'), max_length=150, blank=False, null=False)
     email = models.EmailField(_('email address'), unique=True, blank=False, null=False)
+
+    REQUIRED_FIELDS = ['email', 'first_name', 'last_name']
